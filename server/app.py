21ffc97acbcefce28d72da2a25eaefa4c06e9a09
Ilhamm21 <pratamailham351@gmail.com>
2025-07-31 23:59:10 +0700

    Refactor CORS configuration to simplify allowed origins format

--- server/app.py
+++ server/app.py
@@ -9,9 +9,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": [ 
-    "https://animeai-app.vercel.app"
-]}}, supports_credentials=True)
+CORS(app, resources={r"/*": {"origins": "https://animeai-app.vercel.app"}}, supports_credentials=True)
 
 
 # Koneksi ke MongoDB
