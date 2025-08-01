7a95982bd15167e06ee87db34416272073dafab9
Ilhamm21 <pratamailham351@gmail.com>
2025-08-02 00:53:30 +0700

    Remove unused Procfile and wsgi.py, update app entry point to run Flask app directly on port 8080

--- server/app.py
+++ server/app.py
@@ -307,9 +307,7 @@ def delete_chat_history(user_id, character):
 
 
 
-# if __name__ == '__main__':
-#     import os
-#     port = int(os.environ.get("PORT", 5000))
-#     app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=8080)
 
 
