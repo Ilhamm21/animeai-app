4348af13198c4e2be5c4d66ed03c188683cc69dd
Ilhamm21 <pratamailham351@gmail.com>
2025-08-03 14:30:23 +0700

    perbaikan src vercel

--- server/app.py
+++ server/app.py
@@ -11,7 +11,7 @@ load_dotenv()
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": [
     "http://localhost:5173", 
-    "animeai-app-production.up.railway.app"
+    "https://animeai-app.vercel.app/"
 ]}}, supports_credentials=True)
 
 
