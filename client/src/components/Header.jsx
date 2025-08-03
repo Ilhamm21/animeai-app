350b83b83642fac568928e4fac90c2ae979492b7
Ilhamm21 <pratamailham351@gmail.com>
2025-08-03 15:31:10 +0700

    delet Procile dan perbaikan header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,23 +3,27 @@ import React from 'react';
 const Header = ({ character }) => {
   if (!character) return null;
 
+  const getAvatarSrc = (image) => {
+    if (image.startsWith('http')) return image; // dari backend
+    if (image.startsWith('/avatars')) return image; // path full dari character.js
+    return `/avatars/${image}`; // fallback kalau image cuma nama file
+  };
+
+
   return (
     <header className="flex items-center space-x-3 border-b border-gray-800 px-6 py-3 select-none flex-shrink-0">
       <img
-        src={
-          character.image.startsWith('http')
-            ? character.image
-            : `${process.env.PUBLIC_URL}/avatars/${character.image}`
-        }
+        src={getAvatarSrc(character.image)}
         alt={character.name}
         className="rounded-full"
         width="32"
         height="32"
         onError={(e) => {
           e.target.onerror = null;
-          e.target.src = `${process.env.PUBLIC_URL}/avatars/default.png`;
+          e.target.src = `/avatars/default.png`;
         }}
       />
+
 
 
       <div className="flex flex-col leading-tight">
